7403e2a2280d38254f1bc6ec9b95785d1451b6b0
WOjciech-maker21375 <wojciech.dudus12890@gmail.com>
2026-05-15 16:25:51 +0200

    Update satelity.js

--- categories/satelity.js
+++ categories/satelity.js
@@ -1,7 +1,7 @@
 const satelity = [
  {
     category:"SAT",
-    question: "Jak nazywa się element skupiający sygnał w czaszy?",
+    question: "Jak nazywa się element montowany na ognisku anteny satelitarnej, który odbiera sygnał z satelity, wzmacnia go, a następnie przesuwa jego wysoką częstotliwość na niższy zakres pasma pośredniego (950–2150 MHz), aby mógł być przesłany kablem koncentrycznym do dekodera ?",
     answers: [
      "Konwerter", 
     "Zasilacz", 
